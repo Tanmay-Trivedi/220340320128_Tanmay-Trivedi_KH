39e2f2e4a6070a63cb2c994afc841235d4770218
128_Tanmay Trivedi_PG-DAC-KH <100686400+TanmayTrivedi@users.noreply.github.com>
2022-05-05 17:53:50 +0530

    Add files via upload

---
+++ 220340320128/TwoStacks.java
@@ -0,0 +1,74 @@
+class TwoStacks{
+	int size;
+	int top1,top2;
+	int arr[];
+	
+	TwoStacks(int n){
+	arr=new int[n];
+	size=n;
+	top1=-1;
+	top2=size;
+	}
+	
+	void push1(int x)
+	{
+	if(top1<(top2 -1)){
+	top1++;
+	arr[top1]=x;
+	}
+	else{
+		System.out.println("OVERFLOW");
+		return;
+	}
+	}
+	
+	void push2(int x)
+	{
+	if(top1<(top2 -1)){
+	top2--;
+	arr[top2]=x;
+	}
+	else{
+		System.out.println("OVERFLOW");
+		return;
+	}
+	}
+	
+	int pop1(){
+	if(top1>=0){
+	int x = arr[top1];
+	top1--;
+	return x;
+	}
+	else{
+	System.out.println("UNDERFLOW");
+	System.exit(5);
+	}
+	return 0;
+	}
+	
+	int pop2(){
+	if(top2<size){
+	int x = arr[top2];
+	top2++;
+	return x;
+	}
+	else{
+	System.out.println("UNDERFLOW");
+	System.exit(5);
+	}
+	return 0;
+	}
+	
+	public static void main(String[] args){
+	TwoStacks obj = new TwoStacks(5);
+	obj.push1(5);
+	obj.push2(10);
+	obj.push2(15);
+	obj.push1(11);
+	obj.push2(7);
+	System.out.println("Popped element from stack 1 : "+obj.pop1());
+	obj.push2(40);
+	System.out.println("Popped element from stack 2 : "+obj.pop2());
+	}
+}
